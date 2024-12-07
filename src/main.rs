86378b3f70b56dd14d6b46e585ff0629ca426893
grem-newfield <15900222+grem-newfield@users.noreply.github.com>
2024-12-07 21:27:42 +0200

    added rudimentary menu and states

--- src/main.rs
+++ src/main.rs
@@ -35,6 +35,7 @@ const PIXEL_PERFECT_LAYERS: RenderLayers = RenderLayers::layer(0);
 /// Render layers for high-resolution rendering.
 const HIGH_RES_LAYERS: RenderLayers = RenderLayers::layer(1);
 
+// == CAMERA ==
 #[derive(Component)]
 struct Canvas;
 
@@ -44,12 +45,100 @@ struct InGameCamera;
 #[derive(Component)]
 struct OuterCamera;
 
+// == STATES ==
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
+enum AppState {
+   // Loading,
+   #[default]
+   MainMenu,
+   InGame,
+   Paused,
+   GameOver,
+}
+
+// == MENU ==
+#[derive(Resource)]
+struct MenuData {
+   button_entity: Entity,
+}
+
+const NORMAL_BUTTON_COLOR: Color = Color::srgb(0.5, 0.5, 0.5);
+const HOVERED_BUTTON_COLOR: Color = Color::srgb(0.7, 0.7, 0.7);
+const PRESSED_BUTTON_COLOR: Color = Color::srgb(0.3, 0.3, 0.3);
+
+fn setup_menu(mut cmd: Commands) {
+   let play_button_entity = cmd
+      .spawn(Node {
+         // center button
+         width: Val::Percent(100.0),
+         height: Val::Percent(100.0),
+         justify_content: JustifyContent::Center,
+         align_items: AlignItems::Center,
+         ..Default::default()
+      })
+      .with_children(|parent| {
+         parent
+            .spawn((
+               Button,
+               Node {
+                  width: Val::Px(150.0),
+                  height: Val::Px(65.0),
+                  justify_content: JustifyContent::Center,
+                  align_items: AlignItems::Center,
+                  ..Default::default()
+               },
+               BackgroundColor(NORMAL_BUTTON_COLOR),
+            ))
+            .with_children(|parent| {
+               parent.spawn((
+                  Text::new("Play"),
+                  TextFont { font_size: 30.0, ..Default::default() },
+                  TextColor(Color::WHITE),
+               ));
+            });
+      })
+      .id();
+   cmd.insert_resource(MenuData { button_entity: play_button_entity });
+}
+
+fn menu(
+   mut next_state: ResMut<NextState<AppState>>,
+   mut interaction_query: Query<
+      (&Interaction, &mut BackgroundColor),
+      (Changed<Interaction>, With<Button>),
+   >,
+) {
+   for (inter, mut color) in &mut interaction_query {
+      match *inter {
+         Interaction::Pressed => {
+            *color = PRESSED_BUTTON_COLOR.into();
+            next_state.set(AppState::InGame);
+         }
+         Interaction::Hovered => {
+            *color = HOVERED_BUTTON_COLOR.into();
+         }
+         Interaction::None => {
+            *color = NORMAL_BUTTON_COLOR.into();
+         }
+      }
+   }
+}
+
+fn cleanup_menu(
+   mut cmd: Commands,
+   menu_data: Res<MenuData>,
+) {
+   cmd.entity(menu_data.button_entity).despawn_recursive();
+}
+
+// == /MENU ==
 fn main() {
    App::new()
       .add_plugins((
          DefaultPlugins.set(ImagePlugin::default_nearest()).set(WindowPlugin {
             primary_window: Some(Window {
-               resizable: false,
+               resizable: true,
                decorations: false,
                resolution: WindowResolution::new(1920. / 2., 1080. / 2.),
                // .with_scale_factor_override(1.0),
@@ -58,13 +147,20 @@ fn main() {
             ..default()
          }),
          PhysicsPlugins::default(),
-         PhysicsDebugPlugin::default(),
+         // PhysicsDebugPlugin::default(),
          WorldInspectorPlugin::new(),
       ))
+      .init_state::<AppState>()
       // .add_plugins(())
       // .insert_resource(CameraState { projection_scale: 1.0 })
-      .add_systems(PreStartup, (load_sprites))
-      .add_systems(Startup, (setup_misc, setup_player, setup_camera, setup_weapons, setup_physics))
+      .add_systems(Startup, (load_sprites, setup_cameras))
+      .add_systems(OnEnter(AppState::MainMenu), setup_menu)
+      .add_systems(Update, menu.run_if(in_state(AppState::MainMenu)))
+      .add_systems(OnExit(AppState::MainMenu), cleanup_menu)
+      .add_systems(
+         OnEnter(AppState::InGame),
+         (setup_misc, setup_player, setup_weapons, setup_physics),
+      )
       .add_systems(
          Update,
          (
@@ -73,9 +169,10 @@ fn main() {
             move_player,
             weapons_system,
             fit_canvas_to_window,
-            follow_cam,
+            follow_cam.after(move_player),
             // render_gizmos,
-         ),
+         )
+            .run_if(in_state(AppState::InGame)),
       )
       .run();
 }
@@ -84,7 +181,7 @@ fn render_gizmos(mut gizmos: Gizmos) {
    gizmos.arrow_2d(Vec2::ZERO, Vec2::ONE * 10., bevy::color::palettes::css::YELLOW);
 }
 
-fn setup_camera(
+fn setup_cameras(
    mut cmd: Commands,
    mut images: ResMut<Assets<Image>>,
 ) {
@@ -110,6 +207,7 @@ fn setup_camera(
       Camera2d,
       Camera {
          order: -1,
+         clear_color: ClearColorConfig::Custom(Color::srgb(0.5, 0.75, 0.75)),
          target: bevy::render::camera::RenderTarget::Image(image_handle.clone()),
          ..Default::default()
       },
@@ -118,7 +216,13 @@ fn setup_camera(
       PIXEL_PERFECT_LAYERS,
    ));
    cmd.spawn((Sprite::from_image(image_handle), Canvas, HIGH_RES_LAYERS));
-   cmd.spawn((Camera2d, Msaa::Off, OuterCamera, HIGH_RES_LAYERS));
+   cmd.spawn((
+      Camera2d,
+      Camera { clear_color: ClearColorConfig::Custom(Color::BLACK), ..Default::default() },
+      Msaa::Off,
+      OuterCamera,
+      HIGH_RES_LAYERS,
+   ));
 }
 
 fn fit_canvas_to_window(
@@ -194,38 +298,41 @@ fn keyboard_input(keys: Res<ButtonInput<KeyCode>>) {
 fn move_player(
    time: Res<Time>,
    keys: Res<ButtonInput<KeyCode>>,
-   mut q: Query<&mut Transform, With<Player>>,
-   mut p: Query<&Player>,
+   // mut q: Query<&mut Transform, With<Player>>,
+   // mut p: Query<&Player>,
+   mut transform: Single<&mut Transform, With<Player>>,
+   mut player: Single<&Player>,
 ) {
    // for mut velocity in &mut query {
    //         velocity.y -= 9.8 * DELTA;
    //
-   for (mut transform, player) in zip(&mut q, &p) {
-      // info!("{:?}", transform);
-      // info!("{:?}", player);
-      let mut move_vec = Vec3::ZERO;
-      if keys.pressed(KeyCode::KeyW) {
-         // transform.translation.y += player.speed * time.delta_seconds();
-         move_vec.y += 1.0; // player.speed * time.delta_secs();
-      }
-      if keys.pressed(KeyCode::KeyA) {
-         // transform.translation.x -= player.speed * time.delta_seconds();
-         move_vec.x -= 1.0; //player.speed * time.delta_secs();
-         transform.rotate_z(time.delta_secs() * 3.);
-      }
-      if keys.pressed(KeyCode::KeyS) {
-         // transform.translation.y -= player.speed * time.delta_seconds();
-         move_vec.y -= 1.0; //player.speed * time.delta_secs();
-      }
-      if keys.pressed(KeyCode::KeyD) {
-         // transform.translation.x += player.speed * time.delta_seconds();
-         move_vec.x += 1.0; //player.speed * time.delta_secs();
-         transform.rotate_z(-time.delta_secs() * 3.);
-      }
-      if move_vec != Vec3::ZERO {
-         transform.translation += move_vec.normalize() * player.speed * time.delta_secs();
-      }
+   // for (mut transform, player) in zip(&mut q, &p) {
+   // for (mut transform, player) in zip(&mut q, &p) {
+   // info!("{:?}", transform);
+   // info!("{:?}", player);
+   let mut move_vec = Vec3::ZERO;
+   if keys.pressed(KeyCode::KeyW) {
+      // transform.translation.y += player.speed * time.delta_seconds();
+      move_vec.y += 1.0; // player.speed * time.delta_secs();
    }
+   if keys.pressed(KeyCode::KeyA) {
+      // transform.translation.x -= player.speed * time.delta_seconds();
+      move_vec.x -= 1.0; //player.speed * time.delta_secs();
+      transform.rotate_z(time.delta_secs() * 3.);
+   }
+   if keys.pressed(KeyCode::KeyS) {
+      // transform.translation.y -= player.speed * time.delta_seconds();
+      move_vec.y -= 1.0; //player.speed * time.delta_secs();
+   }
+   if keys.pressed(KeyCode::KeyD) {
+      // transform.translation.x += player.speed * time.delta_seconds();
+      move_vec.x += 1.0; //player.speed * time.delta_secs();
+      transform.rotate_z(-time.delta_secs() * 3.);
+   }
+   if move_vec != Vec3::ZERO {
+      transform.translation += move_vec.normalize() * player.speed * time.delta_secs();
+   }
+   // }
 }
 
 fn follow_cam(
@@ -313,16 +420,18 @@ fn weapons_system(
       if attack_timer.timer.finished() {
          // info!("Timer Lmao");
          // summon projectile
-         c.spawn(
+         let mut ent_cmd = c.spawn(
             ((
                Projectile { speed: 50.0, lifetime: 1.0 },
                Transform::from_xyz(0.0, 0.0, 0.0),
                Sprite::from(projectile_art.basic_projectile.clone()),
                RigidBody::Kinematic,
                Collider::circle(2.0),
-               DebugRender::default().with_collider_color(Color::srgb(0.0, 1.0, 0.0)),
             )),
          );
+         // if cfg!(debug_assertions) {
+         //    ent_cmd.insert(DebugRender::default().with_collider_color(Color::srgb(0.0, 1.0, 0.0)));
+         // }
       }
    }
 }
